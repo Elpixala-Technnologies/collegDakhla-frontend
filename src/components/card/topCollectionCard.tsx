3656834ffec3a8aa249d65d1dd0191c5d14b659d
saurabhp00007 <saurabhpandey@elpixala.in>
2024-05-05 21:58:21 +0530

    changes final~Saurabh

--- src/components/card/topCollectionCard.tsx
+++ src/components/card/topCollectionCard.tsx
@@ -70,7 +70,7 @@ export default function TopCollectionCard({ college, href }: any) {
             </div>
           </div>
           <Link href={href}>
-            <div className="mb-6 ml-2 flex flex-row w-full">
+            <div className="mb-6 px-2 flex flex-row w-full justify-between">
               <span className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner drop-shadow-lg drop-shadow-slate-100 group m-2 ">
                 <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
                 <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
