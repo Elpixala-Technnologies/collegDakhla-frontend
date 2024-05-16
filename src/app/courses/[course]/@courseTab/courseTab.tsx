7eb96bd43eb9fe55ab48c6a89e9097cf046ac616
Agam Mishra <agammishra05@gmail.com>
2024-05-16 15:46:10 +0530

    Develop fix (#25)
    * Develop (#22)
    
    * Chnages ~saurabh
    
    * fixes
    
    * fixes~saurabh
    
    * little animation in homepage ~saurabh
    
    * saurabh-fixes
    
    * HomePages ~saurabh
    
    * remove border from testimonial
    
    * fixes home
    
    * changes
    
    * overflow -fix
    
    * chnages + more page ~Saurabh
    
    * fixes homepage
    
    * remove bg-color
    
    * individual fixes
    
    * faq fixes and some more changes
    
    * New push
    
    * homePage final fixes
    
    * letest One
    
    * all fixes
    
    * Console remove
    
    * changes testing
    
    * Fixed by Ashish
    
    * Ui fixes-aryan
    
    * fixes homepage ~Saurabh
    
    * homePage and individual college page fix ~Saurabh
    
    * changes final~Saurabh
    
    * minor changes
    
    * https://b24-o0ffiw.bitrix24.in/company/personal/user/85/tasks/task/view/263/
    
    * update news
    
    * NewsById fix
    
    * fixed news section
    
    * update news
    
    * fixed filter in news section
    
    * signup otp added
    
    * sign-in otp added
    
    * Related News issues fixed
    
    * sort and filtering ~Pankaj
    
    * network fixed
    
    * update ui
    
    * update ui
    
    * homePage, news , indivisual college nav item fix ~Saurabh
    
    * exam page changes ~Saurabh
    
    * signUpModule~Pankaj
    
    * users meta data create changes
    
    * on successful login route to homepage
    
    * college card update
    
    * update
    
    * update card border
    
    * exams cards
    
    * asdfg~pankaj
    
    * header, signupSlider, footer ~pankaj
    
    * user profile section added
    
    * update course card
    
    * header changes~pankaj
    
    * final chnages ~Saurabh
    
    * fixes
    
    * ui changes~pankaj
    
    * changes
    
    * removed signup page
    
    * loginModule~pankaj
    
    * signUp Module ~pankaj
    
    * Signin/signup (#14)
    
    * signUpModule~Pankaj
    
    * asdfg~pankaj
    
    * header, signupSlider, footer ~pankaj
    
    * changes
    
    * removed signup page
    
    ---------
    
    Co-authored-by: pankaj <er.pankaj,k4@gmail.com>
    
    * signin signup functionality e2e working
    
    * cta modal is done
    
    * changes are done
    
    * cta is done
    
    * build bugs are fixed
    
    * signup fixes
    
    * fixes
    
    * chnages ~Saurabh
    
    * Signin/signup working (#21)
    
    * signup sign in flow correction
    
    * Revert "Merge branch 'develop' of https://github.com/Elpixala-Technnologies/college-dakhla into signin/signup"
    
    This reverts commit 2238095b7578ed7a07e06334691ca3aa24c5be14, reversing
    changes made to 45248b93252868b9d7a14d61ef978346e40d5f1c.
    
    * remove redircetion signin signup
    
    ---------
    
    Co-authored-by: saurabh kumar pandey <saurabhpanday21212@gmail.com>
    Co-authored-by: saurabhp00007 <saurabhpandey@elpixala.in>
    Co-authored-by: Aryan-elpixala <aryan.raj@elpixala.in>
    Co-authored-by: Ashish <ashish@elpixala.in>
    Co-authored-by: Aryan-Elpixala <168053573+Aryan-Elpixala@users.noreply.github.com>
    Co-authored-by: pankaj <er.pankaj,k4@gmail.com>
    Co-authored-by: Saurabh Pandey <168158589+Saurabhp00007@users.noreply.github.com>
    Co-authored-by: Ashim Rudra Paul <codewithashim@gmail.com>
    Co-authored-by: Ashim Rudra Paul <81712334+codewithashim@users.noreply.github.com>
    
    * build changes
    
    ---------
    
    Co-authored-by: saurabh kumar pandey <saurabhpanday21212@gmail.com>
    Co-authored-by: saurabhp00007 <saurabhpandey@elpixala.in>
    Co-authored-by: Aryan-elpixala <aryan.raj@elpixala.in>
    Co-authored-by: Ashish <ashish@elpixala.in>
    Co-authored-by: Aryan-Elpixala <168053573+Aryan-Elpixala@users.noreply.github.com>
    Co-authored-by: pankaj <er.pankaj,k4@gmail.com>
    Co-authored-by: Saurabh Pandey <168158589+Saurabhp00007@users.noreply.github.com>
    Co-authored-by: Ashim Rudra Paul <codewithashim@gmail.com>
    Co-authored-by: Ashim Rudra Paul <81712334+codewithashim@users.noreply.github.com>

--- src/app/courses/[course]/@courseTab/courseTab.tsx
+++ src/app/courses/[course]/@courseTab/courseTab.tsx
@@ -5,6 +5,7 @@ import CollegeData from "@/components/collegeData/collegeData";
 import YoutubeVideo from "@/components/youtubeVideo/youtubeVideo";
 import PageData from "@/components/pageData/pageData";
 import Accordion from "@/components/accordian/accordian";
+import Accordion from "@/components/accordian/accordian";
 
 export default function CourseTab({ tabData }: any) {
 	return (
@@ -15,6 +16,7 @@ export default function CourseTab({ tabData }: any) {
 						<div className="author-section bg-gray-50 rounded-xl p-5">
 							<Author />
 						</div>
+						{/* <div className="page-data-wrapper">
 						{/* <div className="page-data-wrapper">
 							{tabData?.map((item: any, index: number) => {
 								return (
@@ -49,6 +51,7 @@ export default function CourseTab({ tabData }: any) {
 					<div className="right-wrapper basis-1/4 rounded min-w-72 hidden md:block">
 						<div className=" md:flex md:flex-col md:gap-2">
 							{/* <Button
+							{/* <Button
 								href={`/college/`}
 								text="Apply Now"
 								filled
@@ -65,6 +68,7 @@ export default function CourseTab({ tabData }: any) {
 								width="w-full"
 								align="text-center"
 								fontColor="text-primary-text"
+							/> */}
 							/> */}
 							<div className="college-videos bg-gray-50 p-3">
 								<h4 className="text-base font-bold font-poppins border-b-2 pb-2">
