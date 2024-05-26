2ddef9bbf5a0dc9817c972089f0a14d5b29ddad8
Ashim Rudra Paul <codewithashim@gmail.com>
2024-05-27 05:39:26 +0600

    env is connected

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,7 +1,4 @@
-// export const STRAPI_URL = "https://collegedakhla-cms-xtyfl.ondigitalocean.app/graphql";
-// export const restUrl = 'https://collegedakhla-cms-xtyfl.ondigitalocean.app/';
-
-export const STRAPI_URL = "http://localhost:1337/graphql";
-export const restUrl = 'http://localhost:1337';
-
+export const STRAPI_URL = process.env.NEXT_PUBLIC_BASE_GQL_URL || "http://localhost:1337/graphql";
+export const restUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:1337";
+ 
 
